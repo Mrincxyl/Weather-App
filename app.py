959a0e2f8b0e613529fda8144283480a2a6fdbab
MrIncxyl <tsktechnical7@gmail.com>
2025-07-19 21:16:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, session
 import requests
 
@@ -38,3 +39,7 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port) 
